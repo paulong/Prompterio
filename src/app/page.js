42b3c3faff7bbc44c28b58af1f7cfda214e23d89
Paulo <aenemapro@gmail.com>
2026-01-29 17:35:24 -0400

    ADD [adde meta for google serch]

--- src/app/page.js
+++ src/app/page.js
@@ -29,6 +29,7 @@ export default function Home() {
   </div>
 );
 
+
   return (
     <div className="container">
       <div className={styles.blobBlue}></div>
@@ -36,6 +37,7 @@ export default function Home() {
       <Head>
         <title>Prompterio</title>
         <meta name="description" content="Simple, web-based teleprompter app with customizable speed, size, and mirroring." />
+         <meta name="google-site-verification" content="googlefebe8713c01f3b80.html" />
       </Head>
 
       {/* --- 1. Navigation Bar (Dynamic) --- */}
